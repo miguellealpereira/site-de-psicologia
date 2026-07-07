e382996dabb92b3824b8c0beb3ba1e71f5856243
Miguel Leal Pereira <miguellealpereira96@gmail.com>
2026-07-07 18:26:35 -0300

    adiciona arquivo index.css com estilos para imagem de fundo e inclui link para CSS personalizado no index.php

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <!-- conection in Bootstrap 5.3.8 CSS and JS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<!-- Custom CSS -->
+<link rel="stylesheet" href="/site-de-psicologia/style/index.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 <?php
 
 require_once 'includes/header.php';
+
+?>
+
+
 
